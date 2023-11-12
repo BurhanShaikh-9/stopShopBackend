1c8a7737a38638f8feb4f9fb3182c54560f5edce
BurhanShaikh-9 <shaikhshady6@gmail.com>
2023-11-12 03:24:35 -0800

    update

--- src/middleware/jwtCookie.js
+++ src/middleware/jwtCookie.js
@@ -1,21 +1,23 @@
 const jwt = require('jsonwebtoken');
 const { jwtSecret } = require('../../constant')
 
-
 function authenticateToken(req, res, next) {
-    const token = req.headers.authorization;
-
+    const authHeader = req.header('Authorization');
+    const token = authHeader.split(" ")[1];
+    // console.log(token, 'tokeenn');
     if (!token) {
-        return res.sendStatus(401); 
+        return res.status(401).json({ message: 'Unauthorized - Token not found' });
     }
 
-    jwt.verify(token, jwtSecret, (err, user) => {
-        if (err) {
-            return res.sendStatus(403); 
-        }
-        req.user = user;
+    try {
+        const decoded = jwt.verify(token, jwtSecret);
+        // console.log('Token decoded successfully:', decoded);
+        req.user = decoded.user;
         next();
-    });
+    } catch (error) {
+        // console.error('Error verifying token:', error);
+        res.status(401).json({ message: 'Invalid token' });
+    }
 }
 
 module.exports = { authenticateToken };
