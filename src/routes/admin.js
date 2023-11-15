28a2216c780f61a03b611320afec91dcc4b6ba9d
Burhan Ali Shaikh <85751219+Nyx-95@users.noreply.github.com>
2023-11-15 12:14:57 +0500

    update

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const {addAdmin, getAllAdmin, getSingleAdmin} = require('../controller/admin')
 const  multerUpload = require('../middleware/multer')
+const { authenticateToken } = require('../middleware/jwtCookie');
 
-router.post("/admin", multerUpload.single('image'), addAdmin);
-router.get("/admin", getAllAdmin);
-router.get("/admin/:adminId", getSingleAdmin);
+
+
+router.post("/admin", authenticateToken, multerUpload.single('image'), addAdmin);
+router.get("/admin", authenticateToken, getAllAdmin);
+router.get("/admin/:adminId", authenticateToken, getSingleAdmin);
 
 
 module.exports = router;
